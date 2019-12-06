b03318a42ab4d8ed991bb6b29a07b81eab9c6782
CubeDr <spaceship00@naver.com>
2019-12-06 23:10:03 +0900

    ADD: Implement disable maximize on maximize button click

--- src/main/kotlin/discord/titlebar/TitlebarController.kt
+++ src/main/kotlin/discord/titlebar/TitlebarController.kt
@@ -12,11 +12,17 @@ class TitlebarController: Controller() {
     var x: Double = 500.0
     var y: Double = 500.0
 
+    var isFullscreen: Boolean = false
+    var originalX: Double = 0.0
+    var originalY: Double = 0.0
+    var originalWidth: Double = 600.0
+    var originalHeight: Double = 400.0
+
     fun dragged(event: MouseEvent) {
         val stage = event.stage()
+
         stage.x = event.screenX - x
         stage.y = event.screenY - y
-        println("${stage.x}, ${stage.y}")
     }
 
     fun pressed(event: MouseEvent) {
@@ -30,11 +36,28 @@ class TitlebarController: Controller() {
 
     fun maximize(event: MouseEvent) {
         val stage = event.stage()
-        val bounds = Screen.getPrimary().visualBounds
-        stage.x = bounds.minX
-        stage.y = bounds.minY
-        stage.width = bounds.width
-        stage.height = bounds.height
+
+        if(isFullscreen) { // disable fullscreen
+            stage.x = originalX
+            stage.y = originalY
+            stage.width = originalWidth
+            stage.height = originalHeight
+
+            isFullscreen = false
+        } else { // make fullscreen
+            originalX = stage.x
+            originalY = stage.y
+            originalWidth = stage.width
+            originalHeight = stage.height
+
+            val bounds = Screen.getPrimary().visualBounds
+            stage.x = bounds.minX
+            stage.y = bounds.minY
+            stage.width = bounds.width
+            stage.height = bounds.height
+
+            isFullscreen = true
+        }
     }
 
     fun close(event: MouseEvent) {
